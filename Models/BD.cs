62bd16eb5c77cd4c35f20e4025deba79314f679a
JULIIÁN LIFSCHITZ <47575674@ort>
2023-11-07 10:03:21 -0300

    terminar models BD

--- Models/BD.cs
+++ Models/BD.cs
@@ -6,7 +6,45 @@ public static class BD
 {
     private static string _connectionString = @"Server=localhost; DataBase=TPFINAL; Trusted_Connection=True;";
     
-    public static List<Artista> Artistas()
+    public static Usuario Login(string nombre, string contraseña)
+    {
+        Usuario devolver = null;
+        string sql = "Select * From Usuario Where nombre = @pname and Contraseña = @pcon";
+        using(SqlConnection db = new SqlConnection(_connectionString))
+        {
+            devolver = db.QueryFirstOrDefault<Usuario>(sql, new{pname = nombre, pcon = contraseña});
+        }
+        return devolver;
+    }
+
+    public static void Registrarse(Usuario user)
+    {
+        string sql = "Insert into Usuario(nombre, contraseña, mail) Values (@use, @con, @mail)";
+        using(SqlConnection db = new SqlConnection(_connectionString))
+        {
+            db.Execute(sql, new{use = user.nombre, con = user.contraseña, mail = user.gmail});
+        }
+    }
+
+    public static void SeguirArtista(int artistaId, int usuarioId)
+    {
+        string sql = "Insert into ArtistaxUsuario(artistaId, usuarioId) Values (@art, @user)";
+        using(SqlConnection db = new SqlConnection(_connectionString))
+        {
+            db.Execute(sql, new{art = artistaId, user = usuarioId});
+        }
+    }
+
+    public static void DejarSeguirArtista(int artistaId, int usuarioId)
+    {
+        string sql = "Delete from ArtistaxUsuario Where artistaId = @art, usuarioId = @user";
+        using(SqlConnection db = new SqlConnection(_connectionString))
+        {
+            db.Execute(sql, new{art = artistaId, user = usuarioId});
+        }
+    }
+    
+    public static List<Artista> CargarArtistas()
     {
         List<Artista> devolver = null;
         string sql = "Select * From Artista";
@@ -15,5 +53,14 @@ public static class BD
             devolver = db.Query<Artista>(sql).ToList();
         }
         return devolver;
+    }
+
+    public static void Megusta(int artistaId, int usuarioId)
+    {
+        string sql = "Insert into ArtistaxUsuario(artistaId, usuarioId) Values (@art, @user)";
+        using(SqlConnection db = new SqlConnection(_connectionString))
+        {
+            db.Execute(sql, new{art = artistaId, user = usuarioId});
+        }
     }
 }
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -4,4 +4,6 @@ public class Usuario
 {
     public int usuarioId {get;set;};
     public string nombre {get;set;};
+    public string contraseña {get;set;}; 
+    public string gmail {get;set;};
 }
