cabbdee0c4daae04bf11eb91617d8be9d64d06da
Jlavl <150144074+Jlavl@users.noreply.github.com>
2023-11-14 10:42:55 -0300

    Add files via upload

---
+++ wwwroot/DB/procedurestpfinal.sql
@@ -0,0 +1,90 @@
+CREATE PROCEDURE Log_in
+	@pname varchar(999),
+	@pcon varchar(999)
+AS
+BEGIN
+	Select * From Usuario Where nombre = @pname and Contraseña = @pcon;
+END
+GO
+
+CREATE PROCEDURE Registrarse
+	@use varchar(999),
+	@con varchar(999),
+	@mail varchar(999)
+AS
+BEGIN
+	Insert into Usuario(nombre, contraseña, gmail) Values (@use, @con, @mail)
+END
+GO
+
+CREATE PROCEDURE CargarArtistas
+AS
+BEGIN
+Select * From Artistaa
+END
+GO
+
+CREATE PROCEDURE SeguirArtista
+	@art int,
+	@user int
+AS
+BEGIN
+	Insert into ArtistaxUsuario(artistaId, usuarioId) Values (@art, @user)
+END
+GO
+
+--dejar de seguir a un artista // 
+CREATE PROCEDURE DejarSeguirArtista
+	@art int,
+	@user int
+AS
+BEGIN
+	Delete from ArtistaxUsuario Where artistaId = @art and usuarioId = @user
+END
+GO
+
+CREATE PROCEDURE Gusta
+	@art int
+AS
+BEGIN
+	update Artistaa set gusta=gusta+1 where Artistaa.artistaId=@art
+END
+GO
+
+CREATE PROCEDURE NoGusta
+	@art int
+AS
+BEGIN
+	update Artistaa set noGusta=noGusta+1 where Artistaa.artistaId=@art
+END
+GO
+
+CREATE PROCEDURE CargarCanciones
+	@art int
+AS
+BEGIN
+	Select * From Cancion where artistaId = @art
+END
+GO
+
+CREATE PROCEDURE CargarComentarios
+AS
+BEGIN
+Select * From Comentario
+END
+GO
+
+CREATE PROCEDURE AñadirComentario
+	@art int,
+	@userId int,
+	@cont nchar(150)
+AS
+BEGIN
+	Insert into Comentario(artistaId, usuarioId, contenido) Values (@art, @userId, @cont)
+END
+GO
+
+
+
+
+execute Log_in JulianLifsch, 84950
