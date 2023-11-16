2daa79eb48ef186dab52c70f4eba365d7b7a8c90
LUCA LISTINGART MAEKANEHISA <47805160@ort>
2023-11-16 09:07:25 -0300

    js empezado

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿function SeguirArtista(id)
+﻿function SeguirArtista(artistaId, usuarioId)
 {
     $.ajax
     (
@@ -6,13 +6,95 @@
             type: 'POST',
             dataType: 'JSON',
             url: '/Home/SeguirArtista',
-            data: {artistaId: id},
+            data: {artistaId: artistaId, usuarioId: usuarioId},
             success:
                 function(response)
                 {
-                    if (response){$("#boton").html("Dejar de Seguir");}
+                    if (response == 1){$("#boton").html("Dejar de Seguir");}
                     else{$("#boton").html("Seguir");}
+                },
+            error:
+                function(xhr, status)
+                {
+                    alert('Ocurrio un problema');
                 }
         }
     );
+}
+
+function Gusta(artistaId)
+{
+    $.ajax
+    (
+        {
+            type: 'POST',
+            dataType: 'JSON',
+            url: '/Home/Gusta',
+            data: {artistaId: artistaId},
+            success:
+                function(response)
+                {
+                    $("#gusta").html(+ 1);
+                },
+            error:
+                function(xhr, status)
+                {
+                    alert('Ocurrio un problema');
+                }
+        }
+    );
+}
+
+function NoGusta(artistaId)
+{
+    $.ajax
+    (
+        {
+            type: 'POST',
+            dataType: 'JSON',
+            url: '/Home/NoGusta',
+            data: {artistaId: artistaId},
+            success:
+                function(response)
+                {
+                    $("#nogusta").html(+ 1);
+                },
+            error:
+                function(xhr, status)
+                {
+                    alert('Ocurrio un problema');
+                }
+        }
+    );
+}
+
+function ValidarComentario()
+{
+    let leng = document.getElementById("cont").length;
+    if(leng > 0)
+    {
+        function AñadirComentario(artistaId, usuarioId, contenido)
+        {
+            $.ajax
+            (
+                {
+                    type: 'POST',
+                    dataType: 'JSON',
+                    url: '/Home/AñadirComentario',
+                    data: {artistaId: artistaId, usuarioId: usuarioId, contenido: contenido},
+                    success:
+                        function(response)
+                        {
+                            $("#nogusta").html(+ 1);
+                        },
+                    error:
+                        function(xhr, status)
+                        {
+                            alert('Ocurrio un problema');
+                        }
+                }
+            );
+        }
+    }
+    else{alert('no se puede añadir un comentario vacio')}
 }
