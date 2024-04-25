c615d73572c6e627eb6b5d92d20d6c3e1c418140
JULIIÁN LIFSCHITZ <47575674@ort>
2024-04-25 16:32:28 -0300

    denuevo

--- obj/Debug/net7.0/TP-FINAL.AssemblyInfo.cs
+++ obj/Debug/net7.0/TP-FINAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP-FINAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67d86dc4abd164e7fd9c9442e257d8f9305676f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP-FINAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP-FINAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
